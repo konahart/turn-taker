50f97a0ce0e97c9465ccc0ecefdc90d2dcf59933
Kona Goodhart <code@konahart.com>
2020-06-01 00:38:52 -0700

    Move utils to cogs dir

--- bot/cogs/turn_taker.py
+++ bot/cogs/turn_taker.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from collections import defaultdict
 from orderedset import OrderedSet
 from typing import Optional
-from ..utils import join_mentions
+from .utils import join_mentions
 
 
 class TurnTracker(object):
