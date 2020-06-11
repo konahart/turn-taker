9a73f34a3ddc356247b1bc6e8220fbf63c70e79d
Kona Goodhart <code@konahart.com>
2020-06-10 23:46:06 -0700

    Rework bot.py to subclass commands.Bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,21 +1,25 @@
-from discord.ext.commands import Bot
+from discord.ext import commands
 from creds import TOKEN
 from cogs.descended import DescendedFromTheQueenCog
 
+
 COMMAND_PREFIX = '+'
 
-bot = Bot(command_prefix=COMMAND_PREFIX)
-bot.add_cog(DescendedFromTheQueenCog(bot))
+
+class Bot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_cog(DescendedFromTheQueenCog(self))
+
+    async def on_ready(self):
+        print('Logged in as {}#{}!'.format(self.user.name,
+                                           self.user.discriminator))
 
 
-@bot.event
-async def on_ready():
-    print('Logged in as {}#{}!'.format(bot.user.name, bot.user.discriminator))
-
-
-def main():
-    bot.run(TOKEN)
+def main(token):
+    bot = Bot(command_prefix=COMMAND_PREFIX)
+    bot.run(token)
 
 
 if __name__ == "__main__":
-    main()
+    main(TOKEN)
